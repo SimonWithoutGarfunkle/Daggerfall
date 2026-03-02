d274fca240bb5b61be4a4ff1d78774255a93a164
s.lefort <s.lefort@accenture.com>
2026-03-02 16:42:42 +0100

    update pipeline with real tests

---
+++ src/test/java/com/daggerfall/controller/BidouilleControllerIT.java
@@ -0,0 +1,15 @@
+package com.daggerfall.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BidouilleControllerIT {
+
+    @Test
+    @Disabled
+    void testBidon_doitEchouer() {
+        assertThat(1).isEqualTo(2);
+    }
+}
---
+++ src/test/java/com/daggerfall/service/BidouilleTUTest.java
@@ -0,0 +1,15 @@
+package com.daggerfall.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BidouilleTUTest {
+
+    @Test
+    @Disabled
+    void testBidon_doitEchouer() {
+        assertThat(1).isEqualTo(2);
+    }
+}
